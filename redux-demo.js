8fb39f1c4fb56f02486ad60f23a4ddd0c05abb59
vikas9620 <53708079+vikas9620@users.noreply.github.com>
2023-06-12 14:45:13 +0530

    updated

--- redux-demo.js
+++ redux-demo.js
@@ -1,26 +1,38 @@
-const redux = require('redux');
+const redux = require("redux");
 const counterReducer = (state = { counter: 0 }, action) => {
-    switch (action.type) {
-      case 'increment':
-        return {
-          counter: state.counter + 1
-        };
-      case 'decrement':
-        return {
-          counter: state.counter - 1
-        };
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case "increment":
+      return {
+        counter: state.counter + 1,
+      };
+    case "decrement":
+      return {
+        counter: state.counter - 1,
+      };
+    case "INCREMENTBY2":
+      return {
+        counter: state.counter + 2,
+      };
+    case "DECREMENTBY2":
+      return {
+        counter: state.counter - 2,
+      };
+    default:
+      return state;
+  }
+};
 const store = redux.createStore(counterReducer);
 
-const counterSubscriber = ()=>{
-    const latestState = store.getState();
-    console.log(latestState)
-}
-store.subscribe(counterSubscriber)
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+store.subscribe(counterSubscriber);
 for (let i = 0; i < 5; i++) {
-    store.dispatch({ type: 'increment' });
-  }
-  store.dispatch({ type: 'decrement' });
+  store.dispatch({ type: "increment" });
+}
+store.dispatch({ type: "decrement" });
+store.dispatch({ type: "INCREMENTBY2" });
+
+store.dispatch({ type: "DECREMENTBY2" });
+
